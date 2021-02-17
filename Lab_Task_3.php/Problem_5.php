1366639b97ebfb1e02cc01fe074e86cbb4a5c0dd
Aysha Maliha <ayshamaliha15@gmail.com>
2021-02-17 17:10:17 +0600

    Form updated

--- Lab_Task_3.php/Problem_5.php
+++ Lab_Task_3.php/Problem_5.php
@@ -1,5 +1,5 @@
 <?php
-$backColor="green"; $fontColor="red"; $imageWidth="80px"; $imageHeight="80px";
+$backColor="green"; $fontColor="red"; $imageWidth="90px"; $imageHeight="90px";
 ?>
 <img src="aiub_logo.jpg" width="<?php echo $imageWidth;?>" height="<?php echo $imageHeight;?>" />
 <h2>American International University -<span style="background-color:<?php echo $backColor;?>;color:<?php echo $fontColor;?>">
