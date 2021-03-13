27eb6eb2f7b2f918d4b764d67822d873ad1f5dfe
Aysha Maliha <ayshamaliha15@gmail.com>
2021-03-13 16:44:23 +0600

    Assistant User

---
+++ Assistant.php/Assistant_Home_Form.php
@@ -0,0 +1,42 @@
+<?php
+
+?>
+<html>
+	<head></head>
+	<body>
+	    <fieldset style="width:1000px" align="center">
+	    <legend align="center"><center><h1>Hospital Hub</h1></center></legend>
+		<form action="" method="post">
+		<img align="left" src="IMG_20210313_154907.jpg" alt="" height="300px" width="250px">
+			<center><h1>Noor A Aysha</h1></center>
+			<center> <h2>ID:4202</h2>
+			<center> <h2>Assistant of Dr. Farzana Sohael</h2>
+			<center> <h2>Popular Diagnostic Centre,Dhaka</h2><br>
+			<br>
+			<br><br><br>
+			
+			
+			
+			
+			     <h4 style="text-align:left;"><a href="Edit_Profile.php">Edit Profile</a></h5>
+			     <h5 style="text-align:left;"><a href="Chat.php">Chat</a></h5><h5 style="text-align:left;"><a href="Notification.php">Notification</a></h5>
+				 
+				 <h1>Appointment</h1>
+				 
+		
+			
+				<button  onclick="document.location='Education.html'" style="height: 50px; width: 50px"><b><h3>SUN</h3></b> </button>
+				<button  onclick="document.location='Family.html'" style="height: 50px; width: 50px"><b><h3>MON</h3></b> </button>
+				<button  onclick="document.location='Achievement.html'" style="height: 50px; width: 50px"><b><h3>TUE</h3></b></button>
+				<button  onclick="document.location='Experience.html'" style="height: 50px; width: 50px"><b><h3>WED</h3></b></button>
+				<button  onclick="document.location='Education.html'" style="height: 50px; width: 50px"><b><h3>THU</h3></b> </button>
+				<button  onclick="document.location='Family.html'" style="height: 50px; width: 50px"><b><h3>FRI</h3></b> </button>
+				<button  onclick="document.location='Achievement.html'" style="height: 50px; width: 50px"><b><h3>SAT</h3></b></button>
+				
+			
+			<button onclick="document.location='Assistant_Appointment_Request_List_Form.php'"
+	
+		</form>
+		</fieldset>	
+		</body>
+</html>
